28a536a74f90c6517a26509b9a58c651962b0c43
nv95 <110719288+nv95@users.noreply.github.com>
2023-01-17 19:35:09 +0000

    Deploying to gh-pages from @ nv95/goit-js-hw-08@8e5b7ba7461f4e9c9d92b0782ee780a9b1e8a7e4 🚀

--- 03-feedback.af2cf9f9.js
+++
@@ -1,2 +0,0 @@
-const e={form:document.querySelector(".feedback-form"),email:document.querySelector(".feedback-form input"),message:document.querySelector(".feedback-form textarea")};e.form.addEventListener("submit",(function(e){})),e.message.addEventListener("input",(function(e){const t=e.currentTarget.value;localStorage.setItem("feedback-form-state",t)}));
-//# sourceMappingURL=03-feedback.af2cf9f9.js.map
--- 03-feedback.f498bfd4.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={form:document.querySelector(".feedback-form"),email:document.querySelector(".feedback-form input"),message:document.querySelector(".feedback-form textarea")};e.form.addEventListener("submit",(function(e){})),e.message.addEventListener("input",(function(e){var t=e.currentTarget.value;localStorage.setItem("feedback-form-state",t)}))}();
-//# sourceMappingURL=03-feedback.f498bfd4.js.map
